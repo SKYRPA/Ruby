bc512f3bca9fbb136d4406611fa2b7e7942f660f
Kenta_Sugiyama <15kmc08@ms.dendai.ac.jp>
2015-09-07 20:30:22 -0700

    nilを使った演習

---
+++ Ruby 005/004a.rb
@@ -0,0 +1,9 @@
+# coding: windows-31j
+name = nil
+puts "***nil***"
+puts name
+p name
+name = ""
+puts "***empty***"
+puts name
+p name
---
+++ Ruby 005/004b.rb
@@ -0,0 +1,9 @@
+# coding: windows-31j
+name = nil
+puts "***nil***"
+puts name
+p name
+name = "empty"
+puts "***empty***"
+puts name
+p name
