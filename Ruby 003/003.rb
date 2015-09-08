4d5c911e0fc2b3388805ee04a3161ba972266012
Kenta_Sugiyama <15kmc08@ms.dendai.ac.jp>
2015-09-07 17:19:54 -0700

    変数宣言と代入

---
+++ Ruby 003/003.rb
@@ -0,0 +1,9 @@
+# coding: windows-31j
+number=12      #変数numberを作成し、12を代入
+name ="pripara" #変数nameを作成し、「pripara」を代入
+puts number
+puts name
+number = "-----"   #数値12が代入されていたnumberに文字列を代入できる。
+puts number
+number = name   #変数「name」の値を変数「number」に代入
+puts number
