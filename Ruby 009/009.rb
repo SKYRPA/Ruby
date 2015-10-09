0c25294e4e9900294b02d0b88f94512181008f42
Kenta_Sugiyama <15kmc08@ms.dendai.ac.jp>
2015-10-09 16:09:46 -0700

    一部訂正

--- Ruby 009/009.rb
+++ Ruby 009/009.rb
@@ -1,6 +1,6 @@
-value = ["a","b","c","d","e"]
-p value[0]
-p value[2]
-p value[4]
-p value.at(1)
-p value.at(3)
+values = ["a","b","c","d","e"]
+p values[0]
+p values[2]
+p values[4]
+p values.at(1)
+p values.at(3)
